e7327864b8b60bd774b8450dedf2f0515549383b
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-21 11:07:35 +0900

    💄 ListData 이용해 카드 생성 및 카테고리별 카드 img 지정
    map으로 각 카드에 데이터 전달 / 데이터 이용해서 title, category, description 설정 / placehold 이용해서 카테고리별 이미지 색상 다르게 지정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,21 @@ export default function Card({ item }) {
     //2. item의 title, description, category를 각각 h2, p, span태그에 알맞게 넣으세요
     //3. item은 CardList에서 전달됩니다.
     //* 먼저 CardList.jsx를 작업해야 합니다!
+    const imgLink = {
+        기술: `https://placehold.co/600x400/007BFF/FFFFFF?text=Tech&font=poppins`,
+        디자인: `https://placehold.co/600x400/9B59B6/FFFFFF?text=Design&font=poppins`,
+        마케팅: `https://placehold.co/600x400/FFA500/FFFFFF?text=Marketing&font=poppins`,
+    };
 
     return (
         <div className="card">
             <div className="card-image">
-                <img src={`https://placehold.co/600x400`} />
+                <img src={imgLink[item.category]} />
             </div>
             <div className="card-content">
-                <span className="card-category">???</span>
-                <h2 className="card-title">???</h2>
-                <p className="card-description">???</p>
+                <span className="card-category">{item.category}</span>
+                <h2 className="card-title">{item.title}</h2>
+                <p className="card-description">{item.description}</p>
             </div>
         </div>
     );
